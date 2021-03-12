8c33d012ff1feebdbf89ae604f9a951ff693d712
Davis Catherman <dscatherman@wpi.edu>
2021-03-12 14:21:17 -0500

    Add interactive srun script

---
+++ interactive.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# reset getopts
+OPTIND=1
+
+# init vars
+num_cpus=8
+num_gpus=1
+amt_rams=8G
+
+usage() {
+    echo "Usage: $0 [-h] [-c <1|40>] [-g <0|2>] [-r <1G|128G>]
+	-c <int>   :  number of CPUs
+	-g <int>   :  number of GPUs
+	-r <int>G  :  amount of RAM
+"
+}
+
+while getopts ":h:c:g:r:" opt; do
+    case "$opt" in
+    h)
+        usage
+        exit
+        ;;
+    c)  num_cpus=${OPTARG}
+        ;;
+    g)  num_gpus=${OPTARG}
+        ;;
+    r)  amt_rams=${OPTARG}
+        ;;
+    *)
+        usage
+        exit
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${num_cpus}" ] || [ -z "${num_gpus}" ] || [ -z "${amt_rams}" ]; then
+    usage
+    exit
+fi
+
+echo "Parameters:
+	CPUs: ${num_cpus}
+	GPUs: ${num_gpus}
+	RAM:  ${amt_rams}
+"
+
+echo 'srun -n ${num_cpus} --gres=gpu:${num_gpus} --mem ${amt_rams} --job-name=iJob --pty bash
+'
+
+echo "srun -n ${num_cpus} --gres=gpu:${num_gpus} --mem ${amt_rams} --job-name=iJob --pty bash
+"
+
+srun -n ${num_cpus} --gres=gpu:${num_gpus} --mem ${amt_rams} --job-name=iJob --pty bash
